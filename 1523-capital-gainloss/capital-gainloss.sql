6e8cb6b407f8ad4ee77eba5445086520962d1017
Purva Surve <113324497+purvaS98@users.noreply.github.com>
2024-09-03 02:23:20 -0700

    Time: 491 ms (50.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT stock_name,
+SUM(CASE WHEN operation='Buy' THEN -price
+	WHEN operation='Sell' THEN price
+End) 
+AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
